49f30e00cb340939dba977d64f34b2cc25bdc873
Nikitkos19 <114206091+Nikitkos19@users.noreply.github.com>
2023-06-16 17:44:50 +0300

    Create Application.java

---
+++ src/main/java/Application.java
@@ -0,0 +1,16 @@
+public class Application {
+    public static void main(String[] args) {
+        Hello();
+    }
+    public static void Hello(){
+        System.out.println("");
+
+
+    }
+
+
+
+
+
+
+}
