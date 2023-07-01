82d58df35c413bce6b329ca11e77168ca6d33c41
Nikitkos19 <114206091+Nikitkos19@users.noreply.github.com>
2023-07-01 13:20:42 +0300

    1,444

--- src/main/java/com/practice/springpractice_banksystem/BankFrame.java
+++ src/main/java/com/practice/springpractice_banksystem/BankFrame.java
@@ -1,16 +1,18 @@
 package com.practice.springpractice_banksystem;
 
+import lombok.AllArgsConstructor;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 import javax.swing.*;
+@AllArgsConstructor
+public class BankFrame {
 
-public class BankFrame extends JFrame {
-    BankFrame() {
-        JFrame frame = new JFrame("BankApplication");
-        frame.setSize(800, 600);
-        frame.setResizable(false);
-        frame.setLocationRelativeTo(null);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setVisible(true);
-    }
+
+
+
+
+
+
+
+
 
 }
