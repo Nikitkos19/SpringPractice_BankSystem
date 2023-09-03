de9fd599e8dafcee6a8d81b6284f5707ebd9b5b2
Nikitkos19 <114206091+Nikitkos19@users.noreply.github.com>
2023-09-03 17:49:37 +0300

    1.78

--- src/main/java/com/practice/springpractice_banksystem/Properties.java
+++ src/main/java/com/practice/springpractice_banksystem/Properties.java
@@ -1,7 +1,7 @@
 package com.practice.springpractice_banksystem;
 
 public interface Properties {
-    static final String URL = "jdbc:postgresql://localhost:5432/postgres";
+    static final String URL = "jdbc:postgresql://localhost:5433/db";
     static final String USERNAME = "postgres";
-    static final String PASSWORD = "558220";
+    static final String PASSWORD = "009988";
 }
